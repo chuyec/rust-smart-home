16ab0cc0eb3f8c3a0b5d814efbd980c620ac7176
Denis Shreiber <chuyecd@gmail.com>
2022-06-05 23:48:54 +0700

    Home work 2

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,52 @@
 fn main() {
     println!("Hello, world!");
+
+    let outlet = outlet_create(String::from("Outlet in the hall"));
+
+    println!("Outlet: '{}'", outlet.get_description());
+    println!("\tPower consumption: {}", outlet.get_power_consumption());
+    println!("\tIs turned on: {}", outlet.turned_on);
+
+    let thermometer = thermometer_create();
+
+    println!("Thermometer:");
+    println!("\tTemperature: {}", thermometer.get_temperature());
+}
+
+struct Outlet {
+    description: String,
+    power_consumption: u32,
+    turned_on: bool,
+}
+
+impl Outlet {
+    fn get_description(&self) -> &str {
+        &self.description
+    }
+
+    fn get_power_consumption(&self) -> u32 {
+        self.power_consumption
+    }
+}
+
+fn outlet_create(description: String) -> Outlet {
+    Outlet {
+        description,
+        power_consumption: 0,
+        turned_on: false,
+    }
+}
+
+struct Thermometer {
+    temperature: i32,
+}
+
+impl Thermometer {
+    fn get_temperature(&self) -> i32 {
+        self.temperature
+    }
+}
+
+fn thermometer_create() -> Thermometer {
+    Thermometer { temperature: 0 }
 }
